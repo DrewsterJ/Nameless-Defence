a03c7530c98ff203d23d63ffb505fd4707fef1e4
DrewsterJ <drewpjonester@gmail.com>
2023-10-01 16:22:34 -0400

    Updated PlayerController script to accept unity events for user input; Added a speed variable a moveInput variable and a rigidbody variable to assist with player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Stores player movement inputs (WASD or Arrows)
+    private Vector2 moveInput;
+    
+    // Stores player movement speed
+    public int speed;
+
+    private Rigidbody2D rb;
+    
     void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+        Debug.Assert(speed != 0, "Player speed is 0");
+        Debug.Assert(rb != null);
+        Debug.Assert(moveInput != null);
+    }
+    
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        rb.velocity = moveInput.normalized * speed;
+    }
+
+    public void OnMoveInput(InputAction.CallbackContext ctx)
+    {
+        moveInput = ctx.ReadValue<Vector2>();
     }
 }

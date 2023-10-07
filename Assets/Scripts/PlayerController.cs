563f1bffdf3de3c683085243f22e22d9de76f7c7
DrewsterJ <drewpjonester@gmail.com>
2023-10-07 13:09:13 -0400

    Implemented basic player movement and walking/idle animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,14 +6,33 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Stores player movement inputs (WASD or Arrows)
+    // Player movement inputs (WASD or Arrows)
     private Vector2 moveInput;
     
-    // Stores player movement speed
+    // Player movement speed
     public int speed;
-
-    // Stores the rigid body for the Player game object
+    
     private Rigidbody2D rb;
+    public SpriteRenderer sr;
+    public Animator anim;
+    
+    // Player model sprites
+    public Sprite upDown;
+    public Sprite leftRight;
+    public Sprite facingFrontMovingLeft;
+    public Sprite facingFrontMovingRight;
+    public Sprite facingBackMovingLeft;
+    public Sprite facingBackMovingRight;
+    private static readonly int Moving = Animator.StringToHash("Moving");
+
+    private int faceLeftZ = 90;
+    private int faceRightZ = -90;
+    private int faceUpZ = 0;
+    private int faceDownZ = -180;
+    private int faceUpAndRight = -45;
+    private int faceUpAndLeft = 45;
+    private int faceDownAndLeft = 135;
+    private int faceDownAndRight = -135;
     
     void Start()
     {
@@ -26,9 +45,7 @@ public class PlayerController : MonoBehaviour
     }
     
     void Update()
-    {
-        
-    }
+    { }
 
     private void FixedUpdate()
     {
@@ -38,5 +55,75 @@ public class PlayerController : MonoBehaviour
     public void OnMoveInput(InputAction.CallbackContext ctx)
     {
         moveInput = ctx.ReadValue<Vector2>();
+        UpdatePlayerSprite();
+        UpdatePlayerAnim();
+    }
+
+    private void UpdatePlayerAnim()
+    {
+        anim.SetBool(Moving, (moveInput != Vector2.zero));
+    }
+
+    private void UpdatePlayerSprite()
+    {
+        bool movingVertical = moveInput.y != 0;
+        bool movingHorizontal = moveInput.x != 0;
+        bool movingLeft = moveInput.x < -0.1;
+        bool movingDown = moveInput.y < -0.1;
+        bool moving = moveInput != Vector2.zero;
+
+        if (!moving)
+            return;
+
+        // Moving diagonally
+        if (movingVertical && movingHorizontal)
+        {
+            // Moving down and moving left
+            if (movingDown && movingLeft)
+            {
+                sr.transform.rotation = Quaternion.Euler(0, 0, faceDownAndLeft);
+            }
+            // Moving down and moving right
+            else if (movingDown && !movingLeft)
+            {
+                sr.transform.rotation = Quaternion.Euler(0, 0, faceDownAndRight);
+            }
+            // Moving up and moving left
+            else if (!movingDown && movingLeft)
+            {
+                sr.transform.rotation = Quaternion.Euler(0, 0, faceUpAndLeft);
+            }
+            // Moving up and moving right
+            else
+            {
+                sr.transform.rotation = Quaternion.Euler(0, 0, faceUpAndRight);
+            }
+        }
+        // Moving either vertically OR horizontally
+        else
+        {
+            if (movingVertical)
+            {
+                if (movingDown)
+                {
+                    sr.transform.rotation = Quaternion.Euler(0, 0, faceDownZ);
+                }
+                else
+                {
+                    sr.transform.rotation = Quaternion.Euler(0, 0, faceUpZ);
+                }
+            }
+            else if (movingHorizontal)
+            {
+                if (movingLeft)
+                {
+                    sr.transform.rotation = Quaternion.Euler(0, 0, faceLeftZ);
+                }
+                else
+                {
+                    sr.transform.rotation = Quaternion.Euler(0, 0, faceRightZ);
+                }
+            }
+        }
     }
 }

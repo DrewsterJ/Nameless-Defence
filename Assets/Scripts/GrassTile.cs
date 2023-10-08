f8db2f958f44bdce00bc773158aab80062afce94
DrewsterJ <drewpjonester@gmail.com>
2023-10-08 12:12:27 -0400

    Added code for detecting when the player's mouse is hovering over a grass tile

--- Assets/Scripts/GrassTile.cs
+++ Assets/Scripts/GrassTile.cs
@@ -1,18 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GrassTile : MonoBehaviour
 {
+    // Tile components
+    public SpriteRenderer sr;
+    private bool playerMouseIsOverTile;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Assert(sr != null);
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool PlayerMouseIsOverTile()
     {
-        
+        return playerMouseIsOverTile;
+    }
+
+    private void OnMouseEnter()
+    {
+        playerMouseIsOverTile = true;
+    }
+
+    private void OnMouseExit()
+    {
+        playerMouseIsOverTile = false;
     }
 }

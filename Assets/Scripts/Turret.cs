a61d2e1352175a977d327af12db7fdff09842931
DrewsterJ <drewpjonester@gmail.com>
2023-10-28 16:21:53 -0400

    Implemented turret code for following and engaging targets

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -9,31 +9,29 @@ public class Turret : MonoBehaviour
     public int maxHealth;
     public int health;
     public float firingRange; // range the turret can shoot
-
     public GameObject bulletPrefab;
     
     private bool firing;
+    private GameObject activeTarget;
     
     // Start is called before the first frame update
     void Start()
     {
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    public void StartFiring()
-    {
-        firing = true;
-        StartCoroutine(FireAtInterval(firingRate));
+        HandleActiveTargetEngagements();
     }
     
-    public void StopFiring()
+    // Attempt to engage a given target (called by the GameManager)
+    public void TryEngageTarget(GameObject target)
     {
-        firing = false;
+        Debug.Assert(target);
+
+        if (IsWithinRange(target))
+            EngageTarget(target);
     }
     
     // Applies the given amount of damage to the turret
@@ -46,8 +44,54 @@ public class Turret : MonoBehaviour
             KillTurret();
     }
     
+    // Handles engagements with an active target
+    private void HandleActiveTargetEngagements()
+    {
+        if (activeTarget == null)
+            return;
+
+        if (IsWithinRange(activeTarget))
+            AimAtTarget(activeTarget);
+        else
+            DisengageTarget(activeTarget);
+    }
+
+    private void StartFiring()
+    {
+        firing = true;
+        StartCoroutine(FireAtInterval(firingRate));
+    }
+    
+    private void StopFiring()
+    {
+        firing = false;
+    }
+
+    private void AimAtTarget(GameObject target)
+    {
+        if (target == null)
+            return;
+        
+        transform.LookAt(target.transform);
+    }
+
+    private void EngageTarget(GameObject target)
+    {
+        activeTarget = target;
+        StartFiring();
+    }
+    
+    private void DisengageTarget(GameObject target)
+    {
+        if (activeTarget != target) 
+            return;
+
+        StopFiring();
+        activeTarget = null;
+    }
+    
     // Checks whether the given target is within the firingRange of the turret
-    public bool IsTargetWithinRange(GameObject target)
+    private bool IsWithinRange(GameObject target)
     {
         return (Vector2.Distance(transform.position, target.transform.position) <= firingRange);
     }

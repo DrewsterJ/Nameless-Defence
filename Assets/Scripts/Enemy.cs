0959840014e31ada220821d2f5332a458fbbe3d0
DrewsterJ <drewpjonester@gmail.com>
2023-10-29 13:42:32 -0400

    Added basic enemy to the game

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int _maxHealth;
+    public int _health;
+    public int _damage;
+    public int _movementSpeed;
+    public int _attackSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Bullet"))
+        {
+            Debug.Log("Hit by a bullet!");
+            Destroy(other.gameObject);
+        }
+    }
+}

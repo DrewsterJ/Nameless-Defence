bfc6f3ca4a9423ed4a710e2594aea537ab0743eb
DrewsterJ <drewpjonester@gmail.com>
2023-11-12 16:16:26 -0500

    Removed unusued LayerMask from Enemy class

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -12,7 +12,6 @@ public class Enemy : MonoBehaviour
     public int _movementSpeed;
     public int _attackSpeed;
     public float _meleeAttackRange;
-    public LayerMask interactLayerMask;
 
     public GameObject activeTarget;
 

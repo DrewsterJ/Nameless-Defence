2378f8a0e7b1524e4849b9d791c5b83c57726b09
The Real DrewsterJ <88598995+DrewsterJ@users.noreply.github.com>
2023-12-31 13:51:17 -0500

    Performed minor code refactoring and reorganization (#142)

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,15 +6,9 @@ public class Bullet : MonoBehaviour
 {
     public int damage;
     
-    // Start is called before the first frame update
     void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    { }
+    
     void Update()
-    {
-        
-    }
+    { }
 }
